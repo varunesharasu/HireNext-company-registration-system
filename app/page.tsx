8d78d5ed880fe85491e176b2844b69d8d5ff7f40
VaruneshT <tvarunesharasu@gmail.com>
2025-09-16 22:05:29 +0530

    Remove redundant comment from page.tsx
    Deleted an unnecessary comment about importing the main App component and dependencies to clean up the code.

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import "react-phone-input-2/lib/style.css"
 
-// Import the main App component and required dependencies
 import App from "../frontend/src/App"
 import { store } from "../frontend/src/store/store"
 import theme from "../frontend/src/styles/theme"
