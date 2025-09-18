7530dc8facb6843b2752a1ec3965ca652981476a
Varunesh T <tvarunesharasu@gmail.com>
2025-09-18 20:19:06 +0530

    Update jwt.js

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 export const generateToken = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: "90d", // 90-day validity as specified
+    expiresIn: "90d", 
   })
 }
 
